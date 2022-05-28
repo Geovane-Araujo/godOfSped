7e38faf465a0ae84e42f3466e61d98880d6d0bbe
Geovane Araujo <geovane-araujo@outlook.com>
2022-05-28 17:24:23 -0300

    Concluido o Bloco 9 Contribuições

---
+++ GodOfSped/Controller/SpedContribuicoes/Model/Bloco9/Bloco9999.cs
@@ -0,0 +1,16 @@
+﻿
+namespace GodOfSped.Model.SpedContribuicoes.Model;
+
+public class Bloco9999
+{
+    public String REG { get; } = "9999";
+    
+    public String QTD_LIN { get; set; }
+    
+    public String ToString()
+    {
+        String ret = $"|{REG}|{QTD_LIN}|";
+        
+        return ret;
+    }
+}
