1bcf8843474ed40968ab00b7511cb2a48ac81fd3
Geovane Araujo <geovane-araujo@outlook.com>
2022-05-28 17:38:08 -0300

    Concluido Bloco P Contribuições

---
+++ GodOfSped/Controller/SpedContribuicoes/Model/BlocoP/BlocoP199.cs
@@ -0,0 +1,19 @@
+﻿
+namespace GodOfSped.Model.SpedContribuicoes.Model;
+
+public class BlocoP199
+{
+    public String REG { get; } = "P199";
+    
+    public String NUM_PROC { get; set; }
+    
+    public String IND_PROC { get; set; }
+    
+    
+    public String ToString()
+    {
+        String ret = $"|{REG}|{NUM_PROC}|{IND_PROC}|";
+        
+        return ret;
+    }
+}
---
+++ GodOfSped/Controller/SpedContribuicoes/Model/BlocoP/BlocoP990.cs
@@ -0,0 +1,17 @@
+﻿
+namespace GodOfSped.Model.SpedContribuicoes.Model;
+
+public class BlocoP990
+{
+    public String REG { get; } = "P990";
+    
+    public String QTD_LIN_P { get; set; }
+    
+    
+    public String ToString()
+    {
+        String ret = $"|{REG}|{QTD_LIN_P}|";
+        
+        return ret;
+    }
+}
