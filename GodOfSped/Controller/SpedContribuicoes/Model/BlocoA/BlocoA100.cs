e546535d686d61494c0ca9fe1387af439fa8bf6a
Geovane Araujo <geovane-araujo@outlook.com>
2022-05-28 17:07:16 -0300

    Concluido o Bloco A Contribuições

---
+++ GodOfSped/Controller/SpedContribuicoes/Model/BlocoA/BlocoA100.cs
@@ -0,0 +1,55 @@
+﻿
+namespace GodOfSped.Model.SpedContribuicoes.Model;
+
+public class BlocoA100
+{
+    public String REG { get; } = "A100";
+    
+    public String IND_OPER { get; set; }
+    
+    public String IND_EMIT { get; set; }
+    
+    public String COD_PART { get; set; }
+    
+    public String COD_SIT { get; set; }
+    
+    public String SER { get; set; }
+    
+    public String SUB { get; set; }
+    
+    public String NUM_DOC { get; set; }
+    
+    public String CHV_NFSE { get; set; }
+    
+    public String DT_DOC { get; set; }
+    
+    public String DT_EXE_SERV { get; set; }
+    
+    public String VL_DOC { get; set; }
+    
+    public String IND_PGTO { get; set; }
+    
+    public String VL_DESC { get; set; }
+    
+    public String VL_BC_PIS { get; set; }
+    
+    public String VL_PIS { get; set; }
+    
+    public String VL_BC_COFINS { get; set; }
+    
+    public String VL_COFINS { get; set; }
+    
+    public String VL_PIS_RET { get; set; }
+    
+    public String VL_COFINS_RET { get; set; }
+    
+    public String VL_ISS { get; set; }
+    
+    public String ToString()
+    {
+        String ret = $"|{REG}|{IND_OPER}|{IND_EMIT}|{COD_PART}|{COD_SIT}|{SER}|{SUB}|{NUM_DOC}|{CHV_NFSE}|" +
+                     $"{DT_DOC}|{DT_EXE_SERV}|{VL_DOC}|{IND_PGTO}|{VL_DESC}|{VL_BC_PIS}|{VL_PIS}|{VL_BC_COFINS}|{VL_COFINS}|{VL_PIS_RET}|{VL_COFINS_RET}|{VL_ISS}|";
+        
+        return ret;
+    }
+}
---
+++ GodOfSped/Controller/SpedContribuicoes/Model/BlocoA/BlocoA120.cs
@@ -0,0 +1,30 @@
+﻿
+namespace GodOfSped.Model.SpedContribuicoes.Model;
+
+public class BlocoA120
+{
+    public String REG { get; } = "A120";
+    
+    public String VL_TOT_SERV { get; set; }
+    
+    public String VL_BC_PIS { get; set; }
+    
+    public String VL_PIS_IMP { get; set; }
+    
+    public String DT_PAG_PIS { get; set; }
+    
+    public String VL_BC_COFINS { get; set; }
+    
+    public String VL_COFINS_IMP { get; set; }
+    
+    public String DT_PAG_COFINS { get; set; }
+    
+    public String LOC_EXE_SERV { get; set; }
+    
+    public String ToString()
+    {
+        String ret = $"|{REG}|{VL_TOT_SERV}|{VL_BC_PIS}|{VL_PIS_IMP}|{DT_PAG_PIS}|{VL_BC_COFINS}|{VL_COFINS_IMP}|{DT_PAG_COFINS}|{LOC_EXE_SERV}|";
+        
+        return ret;
+    }
+}
